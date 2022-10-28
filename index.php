6501aeee09e5fd9928329446716633e0407c57e9
Jeison Roblero <jroblerog2@miumg.edu.gt>
2022-10-28 00:56:50 -0600

    Desarrollo de Página Principal

--- index.php
+++ index.php
@@ -4,11 +4,119 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imagenes/icono.png" type="image/x-icon">
     <title>Course Administrator</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Fuente -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <header>
-        <h1>Course Administrator</h1>
+        <a href="./" class="left-header">
+            <img class="logo-header" src="imagenes/icono.png" alt="Course Administrator">
+            <h1 class="titulo-header">Administración de Cursos</h1>
+        </a>
+
+        <div class="right-header">
+            <a href="./" class="perfil">
+                <img src="imagenes/perfil.png" alt="Perfil">
+            </a>
+        </div>
     </header>
+
+    <main>
+
+        <div class="panel-cursos">
+            <div class="tarjeta-curso">
+                <div class="curso-img" onclick="window.location.href='./'" alt="Materia">
+                    <img src="https://www.electropolis.es/media/magefan_blog/2017/09/Psychiatric-Disorder-1-1024x768.jpg" alt="Materia">
+                </div>
+
+                <div class="cuerpo-tarjeta">
+                    <a href="./">
+                        <h3 class="nombre-curso">Electrónica Digital</h3>
+                        <h4 class="codigo-curso">220227690030A</h4>
+                        <h5 class="anio-curso">2022-2</h5>
+                    </a>
+                </div>
+                
+            </div>
+
+            <div class="tarjeta-curso">
+                <div class="curso-img" onclick="window.location.href='./'" alt="Materia">
+                    <img src="https://www.electropolis.es/media/magefan_blog/2017/09/Psychiatric-Disorder-1-1024x768.jpg" alt="Materia">
+                </div>
+
+                <div class="cuerpo-tarjeta">
+                    <a href="./">
+                        <h3 class="nombre-curso">Electrónica Digital</h3>
+                        <h4 class="codigo-curso">220227690030A</h4>
+                        <h5 class="anio-curso">2022-2</h5>
+                    </a>
+                </div>
+                
+            </div>
+
+
+            <div class="tarjeta-curso">
+                <div class="curso-img" onclick="window.location.href='./'" alt="Materia">
+                    <img src="https://www.electropolis.es/media/magefan_blog/2017/09/Psychiatric-Disorder-1-1024x768.jpg" alt="Materia">
+                </div>
+
+                <div class="cuerpo-tarjeta">
+                    <a href="./">
+                        <h3 class="nombre-curso">Electrónica Digital</h3>
+                        <h4 class="codigo-curso">220227690030A</h4>
+                        <h5 class="anio-curso">2022-2</h5>
+                    </a>
+                </div>
+                
+            </div>
+
+
+            <div class="tarjeta-curso">
+                <div class="curso-img" onclick="window.location.href='./'" alt="Materia">
+                    <img src="https://www.electropolis.es/media/magefan_blog/2017/09/Psychiatric-Disorder-1-1024x768.jpg" alt="Materia">
+                </div>
+
+                <div class="cuerpo-tarjeta">
+                    <a href="./">
+                        <h3 class="nombre-curso">Electrónica Digital</h3>
+                        <h4 class="codigo-curso">220227690030A</h4>
+                        <h5 class="anio-curso">2022-2</h5>
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+        
+    </main>
+
+    <footer>
+        <div class="container-footer">
+        <a href="./"><img src="imagenes/icono.png" class="logo-footer" alt="Course Administrator"></a>
+            <div class="columna-footer">
+                <h5 class="titulo-footer">Administración de Cursos</h5>
+                <p class="parrafo-footer">Sitio web de control y administración de las actividades de los cursos</p>
+            </div>
+            <div class="columna-footer">
+                <h5 class="titulo-footer">Desarrolladores</h5>
+                <ul>
+                    <li><a class="nombre-desarrollador" href="#!">7690-20-14576 Jeison Roblero</a></li><br>
+                    <li><a class="nombre-desarrollador" href="#!">7690-20-26003 Pablo Garcia</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-copyright">
+            <div>© 2022 Copyright Course Administrator</div>
+            <a class="link-mas-info" href="#!">Más información</a>
+        </div>
+    </footer>
+
 </body>
 </html>
